a29d2caab994bf33f3bf02471618e4b5ce53a2bd
DieRod115 <diegrodriguezroal@gmail.com>
2024-11-24 19:50:06 -0500

    consultas.py

--- init_app.py
+++ init_app.py
@@ -8,7 +8,7 @@ def crear_app():
     app.config['MYSQL_PORT'] = 3306
     app.config['MYSQL_USER']='root'
     app.config['MYSQL_PASSWORD']=''
-    app.config['MYSQL_DB']='flaskcontact'
+    app.config['MYSQL_DB']='rentas_dvds'
     app.secret_key ='mysecretkey'
     mysql.init_app(app)
     from consultas import registrar_consultas
